4edd6ada65ed56d424f0849942d31c870e479a1d
liang shan guang <1648266192@qq.com>
2021-03-20 12:18:12 +0800

    初始化项目

---
+++ backend/src/main/java/com/huawei/magictable/controller/UserController.java
@@ -0,0 +1,10 @@
+/***********************************************************
+ * @Description : 用户管理接口
+ * @author      : 梁山广(Liang Shan Guang)
+ * @date        : 2021/3/20 11:39
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package com.huawei.magictable.controller;
+
+public class UserController {
+}
