5c95d74b47ca4f19eeae4dbdd5701919c5dff82f
liang shan guang <1648266192@qq.com>
2021-03-20 11:25:25 +0800

    初始化项目提交

---
+++ backend/src/main/java/com/huawei/magictable/MagicTableApplication.java
@@ -0,0 +1,13 @@
+package com.huawei.magictable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicTableApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/huawei/magictable/MagicTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huawei.magictable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
