0c09036f8a2cbdd8265981315d0aabecb37c5b91
liang shan guang <1648266192@qq.com>
2021-03-21 23:36:16 +0800

    查询的接口真地写好了

--- backend/src/main/java/com/huawei/magictable/controller/TableController.java
+++ backend/src/main/java/com/huawei/magictable/controller/TableController.java
@@ -33,17 +33,27 @@ public class TableController {
     /**
      * 根据查询条件得到分页查询结果
      * @param table 表名
-     * @param pageNum 第几页
+     * @param pageNum 第几页，从1开始
      * @param pageSize 每页多少条
-     * @param filter 查询语法，参考https://www.runoob.com/mongodb/mongodb-query.html 举例如下：
+     * @param filter 查询语法，参考https://www.runoob.com/mongodb/mongodb-query.html
+     *               举例如下：查询40~70岁，且姓金或鲍的人
      * {
      *     "age": {
      *         "$gt": 40,
      *         "$lt": 70
      *     },
-     *     "driver": {
-     *         "$regex": "金"
-     *     }
+     *     "$or": [
+     *         {
+     *             "driver": {
+     *                 "$regex": "金"
+     *             }
+     *         },
+     *         {
+     *             "driver": {
+     *                 "$regex": "鲍"
+     *             }
+     *         }
+     *     ]
      * }
      * @return 符合条件的记录数
      */
