72c52af4d9054d63f86d0d4046deeb84c463adea
liang shan guang <1648266192@qq.com>
2021-03-23 00:08:13 +0800

    添加插入图标库到mongo中的用例

--- backend/src/test/java/com/huawei/magictable/MongoDBInsertDevicesTest.java
+++ backend/src/test/java/com/huawei/magictable/MongoDBInsertDevicesTest.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
-class MongoDBInsertDataTests {
+class MongoDBInsertDevicesTest {
 
     @Autowired
     MongoTemplate mongoTemplate;
