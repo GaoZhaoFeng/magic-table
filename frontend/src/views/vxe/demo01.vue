39c01502ab8c692d688eb6067fc70d998908031c
liang shan guang <1648266192@qq.com>
2021-03-31 22:17:50 +0800

    表格样式初始化

--- frontend/src/views/vxe/demo01.vue
+++ frontend/src/views/vxe/demo01.vue
@@ -7,60 +7,62 @@
         <vxe-button @click="allAlign = 'right'">居右</vxe-button>
       </template>
     </vxe-toolbar>
-    <vxe-table :align="allAlign" :data="tableData1">
-      <vxe-table-column type="seq" width="60"></vxe-table-column>
-      <vxe-table-column field="name" title="Name"></vxe-table-column>
-      <vxe-table-column field="sex" title="Sex"></vxe-table-column>
-      <vxe-table-column field="age" title="Age"></vxe-table-column>
+    <vxe-table
+      border
+      show-header-overflow
+      show-overflow
+      highlight-hover-row
+      :align="allAlign"
+      :data="tableData"
+    >
+      <vxe-table-column type="seq" title="序号" width="60" />
+      <vxe-table-column field="name" title="姓名" />
+      <vxe-table-column field="sex" title="性别" />
+      <vxe-table-column field="age" title="年龄" />
     </vxe-table>
   </div>
 </template>
 
 <script>
-  import { ref } from 'vue'
   export default {
     name: 'demo01',
-    setup() {
-      const allAlign = ref(null)
-
-      const tableData1 = ref([
-        {
-          id: 10001,
-          name: 'Test1',
-          role: 'Develop',
-          sex: 'Man',
-          age: 28,
-          address: 'vxe-table 从入门到放弃',
-        },
-        {
-          id: 10002,
-          name: 'Test2',
-          role: 'Test',
-          sex: 'Women',
-          age: 22,
-          address: 'Guangzhou',
-        },
-        {
-          id: 10003,
-          name: 'Test3',
-          role: 'PM',
-          sex: 'Man',
-          age: 32,
-          address: 'Shanghai',
-        },
-        {
-          id: 10004,
-          name: 'Test4',
-          role: 'Designer',
-          sex: 'Women ',
-          age: 24,
-          address: 'Shanghai',
-        },
-      ])
-
+    data() {
       return {
-        allAlign,
-        tableData1,
+        allAlign: null,
+        tableData: [
+          {
+            id: 10001,
+            name: 'Test1',
+            role: 'Develop',
+            sex: 'Man',
+            age: 28,
+            address: 'vxe-table 从入门到放弃',
+          },
+          {
+            id: 10002,
+            name: 'Test2',
+            role: 'Test',
+            sex: 'Women',
+            age: 22,
+            address: 'Guangzhou',
+          },
+          {
+            id: 10003,
+            name: 'Test3',
+            role: 'PM',
+            sex: 'Man',
+            age: 32,
+            address: 'Shanghai',
+          },
+          {
+            id: 10004,
+            name: 'Test4',
+            role: 'Designer',
+            sex: 'Women ',
+            age: 24,
+            address: 'Shanghai',
+          },
+        ],
       }
     },
   }
