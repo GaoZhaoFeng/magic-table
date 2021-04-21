32d3a9145122593ded592addb1f63a39b7c5c74f
Beverley <915749634@qq.com>
2021-04-22 02:58:48 +0800

    1.实现mysql的JPA整合； 2.测试JPA-Mysql的CRUD； 3.尝试jsoup进行数据爬取；

--- backend/src/main/java/com/huawei/magictable/enums/ResultEnum.java
+++ backend/src/main/java/com/huawei/magictable/enums/ResultEnum.java
@@ -17,7 +17,8 @@ public enum ResultEnum {
     LOGIN_FAILED(-1, "用户名或者密码错误或用户不存在"),
     GET_INFO_SUCCESS(0, "获取用户信息成功"),
     PARAM_ERR(1, "参数不正确"),
-    GET_PAGE_DATA_SUCCESS(0, "获取指定条件下的分页数据成功");
+    GET_PAGE_DATA_SUCCESS(0, "获取指定条件下的分页数据成功"),
+    POST_BOOK_INFO_SUCCESS(0,"根据URL插入书籍成功");
 
     ResultEnum(Integer code, String message) {
         this.code = code;
